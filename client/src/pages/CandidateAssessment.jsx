cba221c077a8f64100bcaae99953207fe72176f5
davidli82 <davidl52217@gmail.com>
2026-03-11 22:17:15 -0700

    feat: hide starter code until candidate starts assessment

--- client/src/pages/CandidateAssessment.jsx
+++ client/src/pages/CandidateAssessment.jsx
@@ -450,13 +450,6 @@ export default function CandidateAssessment() {
               </div>
             )}
 
-            {/* Starter code files (inline) */}
-            {assessment.starterCodeFiles?.length > 0 && (
-              <div className="mb-6">
-                <StarterCodeIDE files={assessment.starterCodeFiles} readOnly={true} />
-              </div>
-            )}
-
             {/* Warning */}
             <div className="flex items-start gap-3 p-4 bg-yellow-50 border border-yellow-200 rounded-xl mb-6">
               <AlertCircle className="w-5 h-5 text-yellow-600 flex-shrink-0 mt-0.5" />
